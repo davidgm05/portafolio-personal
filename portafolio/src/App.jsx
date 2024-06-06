8a305baaa6ce02faefd29c36aedc07cba7fe09e9
davidgm05 <davidgutierrezmillan05@gmail.com>
2024-06-07 00:22:56 +0200

    first commit

---
+++ portafolio/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import PrincipalSection from './components/PrincipalSection'
+
+function App() {
+ 
+
+  return (
+    <>
+     <PrincipalSection/>
+    </>
+  )
+}
+
+export default App
---
+++ portafolio/src/components/PrincipalSection.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const PrincipalSection = () => {
+  return (
+    <>
+    <div class='container-principar-section'>
+    <div class='title-description-contain'>
+        <h2>Hola me llamo</h2>
+        <h1><b>David</b> y soy <b>Desarrollador Web</b></h1>
+        <p>Soy David Gutiérrez, un desarrollador web junior con una pasión por crear soluciones digitales innovadoras, atractivas, funcionales y aprender y ampliar mis habilidades para mantenerme al día con las últimas tendencias y tecnologías.</p>
+    </div>
+    <div class='my-img-contain'>
+        <div class='circle-img'>
+          <div className='marco1'></div>
+          <div className='marco2'></div>
+        </div>
+        <div class='my-img'></div>
+    </div>
+    
+</div>
+
+    </>
+  )
+}
+
+export default PrincipalSection
